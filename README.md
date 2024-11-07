10df35783dce9779a8ed993a402f95d1471c1c96
thuongnguyenvn <162458044+thng2024@users.noreply.github.com>
2024-11-08 06:08:54 +0700

    data_processing.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fraud_detection
+#Hello from Vietnam
--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,9 @@
 print('adding all the data processing code related fraud detection')
 
+a= 5
+b = 7
+def sum(a, b):
+    c = a+b
+    return c
+print('added a new function sum which is not correct')
+
